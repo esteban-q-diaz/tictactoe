16779a4138ac7d1c84ff4651b4b7ca7d3f31dab2
esteban-q-diaz <estebandpal@gmail.com>
2021-01-04 17:49:24 -0800

    minimum requirements done

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 console.log('works')
 console.dir(document)
 //GLOBAL VARIABLE TO KEEP TRACK OF CLICK COUNTS
-var count = 0;
+var count = 1;
 
+//MODEL
+//MODEL
 //MODEL
 //create a three by three array with all 0s on is
 // add the array to a variable
@@ -27,7 +29,6 @@ for (var i = 0; i < rows ; i++) {
 
 
 function stateChange (string, num1, num2) {
-  // console.log(`State change for ${string} activated`);
   // x = 1, O = 0
   var winner = "no winner yet";
   var player1;
@@ -38,12 +39,6 @@ function stateChange (string, num1, num2) {
     this.currentState[num1][num2] = 2;
   }
 
-
-  //create logic for win or draw
-  //if there are three 1's horizontal player 1 wins
-  //if there are three 2's horizontal player 2 wins
-  //if there are three 1's vertically player 1 wins
-  //if there are three 2's vertically player 2 wins
 
   /* -------Check for horizontal winner -------*/
   for (let i = 0 ; i < this.currentState.length; i++) {
@@ -72,7 +67,6 @@ function stateChange (string, num1, num2) {
 
   for (let i = 0 ; i < arrayVertical.length; i++) {
     var current = arrayVertical[i]
-    // console.log("hiii", current)
       if (current[0] === 1 && current[1] === 1 && current[2] === 1) {
         winner = "Player 1 has Won"
       }
@@ -81,42 +75,122 @@ function stateChange (string, num1, num2) {
       }
   }
 
-  console.log("obj:", arrayVertical)
 
-  // console.log(this.currentState)
+  /* -------Check for diagonal winner -------*/
+  if (this.currentState[0][0] === 1 && this.currentState[1][1] === 1&& this.currentState[2][2] === 1) {
+    winner = "Player 1 has Won"
+  }
+
+  if (this.currentState[0][2] === 1 && this.currentState[1][1] === 1&& this.currentState[2][0] === 1) {
+    winner = "Player 1 has Won"
+  }
+
+  if (this.currentState[0][0] === 2 && this.currentState[1][1] === 2&& this.currentState[2][2] === 2) {
+    winner = "Player 2 has Won"
+  }
+
+  if (this.currentState[0][2] === 1 && this.currentState[1][1] === 1&& this.currentState[2][0] === 1) {
+    winner = "Player 2 has Won"
+  }
+
+  /* -------Check for draws -------*/
+  if(count === 9) {
+    winner = 'draw'
+  }
+
+
+  console.log(this.currentState)
   this.updateDOM(string, winner)
 }
 
 
 //VIEW
-//function that is activated when there is a change to state in the model
+//VIEW
+//VIEW
 
-// this section will be connected to the DOM
-// it will change the appearence of the DOM grid
-// if info says game is over then that will be sent to the DOM
 function updateDOM (string, winner) {
 
+  // Get access to the incoming class String (access to the DOM)
   var currentState = document.getElementsByClassName(string)
+  // Locate where the content of the incoming class is found
   var changeState = currentState[0].childNodes[0];
   if (this.count % 2 === 0) {
     changeState.data = 'X'
   } else {
     changeState.data = 'O'
   }
+
+  if (winner === "Player 1 has Won") {
+    //stop the game
+    setTimeout(function (){
+      alert('Player 1 has won the game')
+      this.resetGame()
+    }, 50)
+  }
+
+  if (winner === "Player 2 has Won") {
+    //stop the game
+    setTimeout(function (){
+      alert('Player 2 has won the game')
+      this.resetGame()
+    }, 50)
+  }
+
+  if (winner === "draw") {
+    //stop the game
+    setTimeout(function (){
+      alert('Game has ended in a tie')
+      this.resetGame()
+    }, 50)
+  }
+
   console.log("this is the count", this.count, winner)
 }
 
 
+/* --------- Reset the game --------------*/
+function resetGame() {
+  console.log('reset the game')
+  // reset the Matrix back to 0's
+  const rows = 3;
+  const columns = 3;
+
+  for (var i = 0; i < rows ; i++) {
+      this.currentState[i] = [0]
+    for (var j = 0; j < columns; j++) {
+      this.currentState[i][j] = 0
+    }
+  }
+
+  //reset DOM elements back to blanks
+  const parent = document.getElementsByClassName('table')
+  let child_nodes = parent[0].childNodes
+
+  var innerFunc = function(array) {
+    console.log("tr",array)
+    for (var i = 0; i < array.length; i++) {
+        console.log("array at nodes", array[i])
+        if(array[i].data === "X" || array[i].data === "O" ) {
+          array[i].data = ''
+        }
+
+        if(array[i].data === undefined) {
+          innerFunc(array[i].childNodes)
+      }
+
+    }
+  }
+ innerFunc(child_nodes)
+
+ count = 1;
+}
+
 
 //CONTROLLER
+//CONTROLLER
+//CONTROLLER
 
-//onCLick event function that changes state anytime something in the DOM tic tac toe grid is clicks
-
-//OnClick -> invoke a function that invookes the model function passing in information as to what was clicked
-
-
-// CLICK FOR BOX ONE
-
+//CLICK FOR BOX ONE
 const one = document.querySelector('.one')
 
 one.addEventListener('click', event=> {
@@ -195,4 +269,11 @@ nine.addEventListener('click', event=> {
   console.log('click three worked')
   this.stateChange('nine', 2, 2)
   this.count++
+})
+
+
+const resetButton = document.querySelector('.resetBtn')
+
+resetButton.addEventListener('click', event => {
+  resetGame()
 })
